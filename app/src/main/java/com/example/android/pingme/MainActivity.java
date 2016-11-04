f273158cb877246046ee3f564c0674ac13a7f0d0
Soichiro-Yoshimura <yoshimura@soichiro.org>
2016-11-04 18:37:31 +0900

    mend
    Add Regular Activity

--- app/src/main/java/com/example/android/pingme/MainActivity.java
+++ app/src/main/java/com/example/android/pingme/MainActivity.java
@@ -4,6 +4,7 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -17,21 +18,19 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onPingClick(View v) {
+        Intent resultIntent = new Intent(this, ResultActivity.class);
+        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
+        stackBuilder.addParentStack(ResultActivity.class);
+        // Adds the Intent to the top of the stack
+        stackBuilder.addNextIntent(resultIntent);
+        PendingIntent resultPendingIntent =
+                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
+
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(this)
                         .setSmallIcon(R.drawable.ic_stat_notification)
                         .setContentTitle("My notification")
                         .setContentText("Hello World!");
-
-        Intent resultIntent = new Intent(this, ResultActivity.class);
-        PendingIntent resultPendingIntent =
-                PendingIntent.getActivity(
-                        this,
-                        0,
-                        resultIntent,
-                        PendingIntent.FLAG_UPDATE_CURRENT
-                );
-
         mBuilder.setContentIntent(resultPendingIntent);
         NotificationManager mNotifyMgr =
                 (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
