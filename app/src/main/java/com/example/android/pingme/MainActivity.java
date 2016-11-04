3fe410442a1399e8cb5d7b83826102938e38777e
Soichiro-Yoshimura <yoshimura@soichiro.org>
2016-11-04 18:44:02 +0900

    Add Special Activity

--- app/src/main/java/com/example/android/pingme/MainActivity.java
+++ app/src/main/java/com/example/android/pingme/MainActivity.java
@@ -4,7 +4,6 @@ import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
 import android.support.v4.app.NotificationCompat;
-import android.support.v4.app.TaskStackBuilder;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -18,20 +17,31 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onPingClick(View v) {
-        Intent resultIntent = new Intent(this, ResultActivity.class);
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(this);
-        stackBuilder.addParentStack(ResultActivity.class);
-        // Adds the Intent to the top of the stack
-        stackBuilder.addNextIntent(resultIntent);
-        PendingIntent resultPendingIntent =
-                stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
-
         NotificationCompat.Builder mBuilder =
                 new NotificationCompat.Builder(this)
                         .setSmallIcon(R.drawable.ic_stat_notification)
                         .setContentTitle("My notification")
                         .setContentText("Hello World!");
-        mBuilder.setContentIntent(resultPendingIntent);
+
+        // Creates an Intent for the Activity
+        Intent notifyIntent =
+                new Intent(this, ResultActivity.class);
+        // Sets the Activity to start in a new, empty task
+        notifyIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK |
+                Intent.FLAG_ACTIVITY_CLEAR_TASK);
+        // Creates the PendingIntent
+        PendingIntent pendingIntent =
+                PendingIntent.getActivity(
+                        this,
+                        0,
+                        notifyIntent,
+                        PendingIntent.FLAG_UPDATE_CURRENT
+                );
+
+        // Puts the PendingIntent into the notification builder
+        mBuilder.setContentIntent(pendingIntent);
+        // Notifications are issued by sending them to the
+        // NotificationManager system service.
         NotificationManager mNotifyMgr =
                 (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         // Builds the notification and issues it.
