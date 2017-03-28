65c31897367cfabda631c545aac1fea7798425e8
Soichiro-Yoshimura <yoshimura@soichiro.org>
2017-03-28 16:59:35 +0900

    Add Highlevel challenge answer

--- app/src/main/java/com/example/android/pingme/PingService.java
+++ app/src/main/java/com/example/android/pingme/PingService.java
@@ -112,43 +112,12 @@ public class PingService extends IntentService {
         startTimer(mMillis);
     }
 
-    private void issueNotification(final NotificationCompat.Builder builder) {
+    private void issueNotification(NotificationCompat.Builder builder) {
         mNotificationManager = (NotificationManager)
                 getSystemService(NOTIFICATION_SERVICE);
-
-        // Start a lengthy operation in a background thread
-        new Thread(
-                new Runnable() {
-                    @Override
-                    public void run() {
-                        int incr;
-                        // Do the "lengthy" operation 20 times
-                        for (incr = 0; incr <= 100; incr += 5) {
-                            // Sets the progress indicator to a max value, the
-                            // current completion percentage, and "determinate"
-                            // state
-                            builder.setProgress(100, incr, false);
-                            // Displays the progress bar for the first time.
-                            mNotificationManager.notify(CommonConstants.NOTIFICATION_ID, builder.build());
-                            // Sleeps the thread, simulating an operation
-                            // that takes time
-                            try {
-                                // Sleep for 5 seconds
-                                Thread.sleep(5 * 1000);
-                            } catch (InterruptedException e) {
-                                Log.d("issueNotification", "sleep failure");
-                            }
-                        }
-                        // When the loop is finished, updates the notification
-                        builder.setContentText("Download complete")
-                                // Removes the progress bar
-                                .setProgress(0, 0, false);
-                        mNotificationManager.notify(CommonConstants.NOTIFICATION_ID, builder.build());
-                    }
-                }
-                // Starts the thread by calling the run() method in its Runnable
-        ).start();
-
+        // Including the notification ID allows you to update the notification later on.
+        builder.setProgress(0, 0, true);
+        mNotificationManager.notify(CommonConstants.NOTIFICATION_ID, builder.build());
     }
 
     // Starts the timer according to the number of seconds the user specified.
